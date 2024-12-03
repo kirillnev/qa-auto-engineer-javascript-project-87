fe1d487519ead4ba60b37355b11987bc25408bc7
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2024-12-03 11:17:33 +0500

    Шаг 4: Сравнение плоских файлов

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,8 +1,25 @@
 import parseFile from './utils/fileParser.js';
 
-export default function genDiff(filepath1, filepath2) {
+const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  return `Comparing files:\n${JSON.stringify(data1, null, 2)}\n${JSON.stringify(data2, null, 2)}`;
-}
+  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
+
+  const result = keys.map((key) => {
+    if (!(key in data2)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!(key in data1)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
