f93db48cd0a2e110ad58957e0b10880c14f872c2
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2024-12-06 22:43:54 +0500

    chores: add ignore dirs fot eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,10 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
+    ignores: [
+      "node_modules/**",
+      "__fixtures__/**",
+      "coverage/**"], // Исключаемые папки
     languageOptions: {
       globals: {
         ...globals.browser, // Существующие глобальные переменные браузера
