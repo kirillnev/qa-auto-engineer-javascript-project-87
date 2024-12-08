3e1aa512b1f74172ab4b92a248cab8ec3c375cc9
Kirill Nevinskiy <k.nevinskiy@tbank.ru>
2024-12-08 15:31:28 +0500

    feature: add yaml file format

--- src/utils/dataParser.js
+++ src/utils/dataParser.js
@@ -1,7 +1,12 @@
+import YAML from 'yaml';
+
 const parseData = (content, format) => {
   switch (format) {
     case '.json':
       return JSON.parse(content);
+    case '.yml':
+    case '.yaml':
+      return YAML.parse(content);
     default:
       throw new Error(`Unsupported file format: ${format}`);
   }
